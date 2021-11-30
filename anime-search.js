47b7e6c6a00305d0447b2862fbd5dc9cd44236f7
Isam-AlBitar-NTI <isam.albitar@elev.ga.ntig.se>
2021-11-30 10:55:03 +0100

    börjat med searchbar javascript och skapat anime-search.js.

---
+++ anime-search.js
@@ -0,0 +1,9 @@
+const anime = [
+    { name: 'attack on titan' },
+    { name: 're:zero' },
+    { name: 'my hero academia' },
+    { name: 'naruto' },
+    { name: 'bleach' },
+    { name: 'seven deadly sins' },
+    { name: 'baki' },
+];
