f4d33265a6b13e8fe0b2b9f06bc9a496eeab9ddd
Isam-AlBitar-NTI <isam.albitar@elev.ga.ntig.se>
2021-11-30 22:48:02 +0100

    arbetat med javascript klart, men funkar inte, letar efter fel.

--- anime-search.js
+++ anime-search.js
@@ -5,5 +5,61 @@ const anime = [
     { name: 'naruto' },
     { name: 'bleach' },
     { name: 'seven deadly sins' },
-    { name: 'baki' },
+    { name: 'baki' }
 ];
+
+const list = document.getElementById('list');
+
+function setList(group) {
+    clearList();
+    for (const series of group) {
+        const item = document.createElement('li');
+        item.classList.add('list-group-item');
+        const text = document.createTextNode(series.name);
+        item.appendChild(text);
+        list.appendChild(item);
+    }
+    if (group.length === 0) {
+        setNoResults();
+    }
+}
+
+function clearList() {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function setNoResults() {
+    const item = document.createElement('li');
+item.classList.add('list-group-item');
+    const text = document.TextNode('No results found');
+    item.appendChild(text);
+    list.appendChild(item);
+}
+
+function getRelevancy(value, searchTerm) {
+    if (value === searchTerm) {
+        return 2;
+    } else if (value.startWith(searchTerm)) {
+        return 1;
+    } else if (value.includes(searchTerm)){
+        return 0;
+    }
+}
+
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('input', (event) => {
+    let value = event.target.value;
+    if (value && value.trim().length > 0) {
+        value = value.trim().toLowerCase();
+        setList(anime.filter(animeName => {
+            return animeName.name.includes(value);
+        }).sort((animeA, animeB) => {
+            return getRelevancy(animeB.name, value) - getRelevancy(animeA.name, value);
+        }));
+    } else {
+        clearList();
+    }
+})
